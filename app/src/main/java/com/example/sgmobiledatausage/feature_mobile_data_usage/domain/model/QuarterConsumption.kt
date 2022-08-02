e2cc9aff7010e90ccebbeb0547edd3a3f6d606e3
Sanath <sanathsajeevakumara@gmail.com>
2022-08-02 14:36:58 +0530

    Duplicate data issue is fixed

--- app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/domain/model/QuarterConsumption.kt
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/domain/model/QuarterConsumption.kt
@@ -3,9 +3,9 @@ package com.example.sgmobiledatausage.feature_mobile_data_usage.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = ["year", "quarter"])
 data class QuarterConsumption(
-    @PrimaryKey val id: Int? = null,
+//    @PrimaryKey val id: Int? = null,
     val year: Int,
     val quarter: String,
     val volume: Float
