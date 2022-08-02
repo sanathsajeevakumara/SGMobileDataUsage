292def89e5bf870ac578d4bcd24c3e163bc76de8
Sanath <sanathsajeevakumara@gmail.com>
2022-08-02 09:45:04 +0530

    YearlyConsumptionItem class and String resources are created and added

---
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/presentation/yearly_total_consumption/components/YearlyConsumptionItem.kt
@@ -0,0 +1,44 @@
+package com.example.sgmobiledatausage.feature_mobile_data_usage.presentation.yearly_total_consumption.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.example.sgmobiledatausage.R
+
+@Composable
+fun YearlyConsumptionItem(
+    year: String,
+    totalConsumption: String,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.year) + " $year",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.total_consumption) +  " $totalConsumption",
+                fontStyle = FontStyle.Italic,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+
+}
