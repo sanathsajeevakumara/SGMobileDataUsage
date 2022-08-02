f3d60dca88ade29e4124731d7d935c2c856a05d2
Sanath <sanathsajeevakumara@gmail.com>
2022-08-02 08:59:48 +0530

    Models are created in Domain layer

---
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/domain/model/QuarterConsumption.kt
@@ -0,0 +1,12 @@
+package com.example.sgmobiledatausage.feature_mobile_data_usage.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class QuarterConsumption(
+    @PrimaryKey val id: Int? = null,
+    val year: Int,
+    val Quarter: String,
+    val volume: Float
+)
---
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/domain/model/YearlyTotalConsumption.kt
@@ -0,0 +1,6 @@
+package com.example.sgmobiledatausage.feature_mobile_data_usage.domain.model
+
+data class YearlyTotalConsumption(
+    val year: Int,
+    val totalConsumption: Float
+)
