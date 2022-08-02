5716a24cf1ee441aa2861b82cc60261df1325659
Sanath <sanathsajeevakumara@gmail.com>
2022-08-02 09:03:09 +0530

    Mobile Data usage repository is created

--- app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/data/local/MobileDataUsageDao.kt
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/data/local/MobileDataUsageDao.kt
@@ -13,16 +13,16 @@ import kotlinx.coroutines.flow.Flow
 interface MobileDataUsageDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllDataUsageRecords(usage: List<QuarterConsumption>)
+    suspend fun insertAllMobileDataUsageRecord(usage: List<QuarterConsumption>)
 
     @Query("SELECT * FROM quarterconsumption")
-    fun getAllQuarterUsage(): Flow<List<QuarterConsumption>>
+    fun getAllQuarterConsumption(): Flow<List<QuarterConsumption>>
 
     @Query("SELECT year,SUM(volume) as totalUsage From quarterconsumption GROUP BY year")
-    fun getYearlyUsage(): Flow<List<YearlyTotalConsumption>>
+    fun getYearlyTotalConsumption(): Flow<List<YearlyTotalConsumption>>
 
     @Query("SELECT year, group_concat(volume) as volumes , group_concat(quarter) as quarters FROM quarterconsumption GROUP BY year")
-    suspend fun getGroupedQuarterUsage():List<FilteredQuarterConsumption>
+    suspend fun getFilteredQuarterConsumption():List<FilteredQuarterConsumption>
 
     @Query("SELECT year from quarterconsumption ORDER BY year ASC LIMIT 1")
     suspend fun getInitialYear():Int
