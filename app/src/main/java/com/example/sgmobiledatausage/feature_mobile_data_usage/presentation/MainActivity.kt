988b12e3c63688c549ff35137c5b251054a8ffd0
Sanath <sanathsajeevakumara@gmail.com>
2022-08-02 11:10:19 +0530

    DestinationNav Host is added and Yearly Consumption screen is working correctly

--- app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sgmobiledatausage/feature_mobile_data_usage/presentation/MainActivity.kt
@@ -7,19 +7,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.sgmobiledatausage.feature_mobile_data_usage.presentation.yearly_total_consumption.NavGraphs
 import com.example.sgmobiledatausage.ui.theme.SGMobileDataUsageTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalMaterial3Api
+@ExperimentalPagerApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SGMobileDataUsageTheme {
-
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
     }
